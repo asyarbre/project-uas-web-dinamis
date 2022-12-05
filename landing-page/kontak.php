4070956b6324295beb3b1695dca7fafcacd6fd42
Asya'ri ‎ <asyari818@gmail.com>
2022-12-05 20:29:54 +0700

    Landing page: Add kontak

---
+++ landing-page/kontak.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Tokopedia</title>
+  <link rel="stylesheet" href="../dist/style.css" />
+  <link rel="icon" type="image/x-icon" href="../../src/img/logo.png" />
+  <link href="../vendor/aos/aos.css" rel="stylesheet" />
+</head>
+
+<body>
+  <?php include("../components/landing-nav.php"); ?>
+
+  <section class="bg-gray-50 py-20">
+    <hr class="bg-primary h-0.5 w-10 mx-auto" />
+    <h2 class="mt-3 text-2xl font-semibold text-gray-800 text-center md:text-4xl lg:text-4xl lg:w-full">
+      Kontak Kami
+    </h2>
+    <div class="container mt-10 px-5 mx-auto max-w-lg md:max-w-2xl md:flex md:flex-wrap md:justify-around lg:max-w-4xl">
+      <div class="w-full md:w-72 lg:w-96 my-5 px-5 py-10 rounded-lg border border-gray-300 hover:-translate-y-3 hover:shadow-xl hover:border-primary" data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="1000">
+        <h4 class="font-bold text-gray-700 text-xl md:text-2xl">
+          Pertanyaan Pers
+        </h4>
+        <p class="text-gray-500 my-2 md:text-lg">
+          Untuk keperluan pers dan media, kunjungi
+          <span class="text-primary font-medium">Ruang berita</span>
+        </p>
+      </div>
+      <div class="w-full md:w-72 lg:w-96 my-5 px-5 py-10 rounded-lg border border-gray-300 hover:-translate-y-3 hover:shadow-xl hover:border-primary" data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="1000">
+        <h4 class="font-bold text-gray-700 text-xl md:text-2xl">
+          Permintaan Pembicara
+        </h4>
+        <p class="text-gray-500 my-2 md:text-lg">
+          Permintaan pembicara untuk mengisi acara Anda. Mohon sertakan
+          proposal serta detail acara dan kirim melalui email ke
+          <span class="text-primary font-medium">speakers@tokopedia.com</span>
+        </p>
+      </div>
+      <div class="w-full md:w-72 lg:w-96 my-5 px-5 py-10 rounded-lg border border-gray-300 hover:-translate-y-3 hover:shadow-xl hover:border-primary" data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="1000">
+        <h4 class="font-bold text-gray-700 text-xl md:text-2xl">
+          Tokopedia Care
+        </h4>
+        <p class="text-gray-500 my-2 md:text-lg">
+          Jika Anda memiliki pertanyaan seputar transaksi atau membutuhkan
+          bantuan lainnya, kunjungi
+          <span class="text-primary font-medium">Tokopedia Care</span>
+        </p>
+      </div>
+      <div class="w-full md:w-72 lg:w-96 my-5 px-5 py-10 rounded-lg border border-gray-300 hover:-translate-y-3 hover:shadow-xl hover:border-primary" data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="1000">
+        <h4 class="font-bold text-gray-700 text-xl md:text-2xl">Karir</h4>
+        <p class="text-gray-500 my-2 md:text-lg">
+          Ingin ikut andil dalam pemerataan ekonomi secara digital? Kunjungi
+          situs
+          <span class="text-primary font-medium">Karir Kami</span>
+        </p>
+      </div>
+    </div>
+  </section>
+  <section class="bg-gray-100 py-20">
+    <hr class="bg-primary h-0.5 w-10 mx-auto" />
+    <h2 class="mt-3 text-2xl font-semibold text-gray-800 text-center md:text-4xl lg:text-4xl lg:w-full">
+      Kantor Kami
+    </h2>
+    <div class="container mt-10 w-full px-10 md:flex sm:mx-auto xl:max-w-6xl sm:gap-6">
+      <div class="my-5 md:w-1/2 border rounded-lg bg-white hover:-translate-y-3 hover:shadow-xl hover:border-primary" data-aos="zoom-in-up" data-aos-duration="1000">
+        <a href="https://goo.gl/maps/6riwr9QPwARMEPe2A" target="_blank">
+          <img src="../src/img/maps-1.jpg" alt="lokasi-1" width="w-full" />
+          <div class="flex px-4 mt-5">
+            <img src="../src/img/maps.svg" alt="maps-icon" class="rounded-xl" />
+            <p class="my-auto ml-3 font-semibold text-gray-700 text-lg">
+              Tokopedia Care
+            </p>
+          </div>
+          <p class="mx-3 my-3 px-5 text-sm leading-loose">
+            TOKOPEDIA CARE TOWER - Ground Floor Ciputra International, Jl.
+            Lkr. Luar Barat No.101, RT.13, Rw. Buaya, Kecamatan Cengkareng,
+            Kota Jakarta Barat, Daerah Khusus Ibukota Jakarta 11740
+          </p>
+        </a>
+      </div>
+      <div class="my-5 md:w-1/2 border rounded-lg bg-white hover:-translate-y-3 hover:shadow-xl hover:border-primary" data-aos="zoom-in-up" data-aos-duration="1000">
+        <a href="https://goo.gl/maps/HxQE3tvNEuR2QJFg7" target="_blank">
+          <img src="../src/img/maps-2.jpg" alt="lokasi-1" width="w-full" />
+          <div class="flex px-4 mt-5">
+            <img src="../src/img/maps.svg" alt="maps-icon" class="rounded-xl" />
+            <p class="my-auto ml-3 font-semibold text-gray-700 text-lg">
+              Tokopedia Tower
+            </p>
+          </div>
+          <p class="mx-3 my-3 px-5 text-sm leading-loose">
+            Tokopedia Tower Ciputra World 2, Jl. Prof. DR. Satrio No.Kav. 11,
+            RT.3/RW.3, Karet Semanggi, Setia Budi, Kota Jakarta Selatan,
+            Daerah Khusus Ibukota Jakarta 12950
+          </p>
+        </a>
+      </div>
+    </div>
+  </section>
+  <?php include("../components/footer.php"); ?>
+  <script src="../script.js"></script>
+  <script src="../vendor/aos/aos.js"></script>
+  <script>
+    AOS.init();
+  </script>
+</body>
+
+</html>

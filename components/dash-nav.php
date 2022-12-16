4e50bb719eb1ce60910403c95c482a7bed1c01f9
Asya'ri ‎ <asyari818@gmail.com>
2022-12-16 13:37:21 +0700

    Edit Navigation Dashboard

--- components/dash-nav.php
+++ components/dash-nav.php
@@ -1,6 +1,6 @@
-<?php $activePage = basename($_SERVER['PHP_SELF'], ".php"); 
-$base_url="http://localhost/web-dinamis-uas/dashboard/";
-$landing_url="http://localhost/web-dinamis-uas/landing-page/";
+<?php $activePage = basename($_SERVER['PHP_SELF'], ".php");
+$base_url = "http://localhost/web-dinamis-uas/dashboard/";
+$landing_url = "http://localhost/web-dinamis-uas/landing-page/";
 ?>
 
 <li class="nav-item">
@@ -11,33 +11,43 @@ $landing_url="http://localhost/web-dinamis-uas/landing-page/";
   <div id="collapseTwo" class="collapse show" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
     <div class="bg-white py-2 collapse-inner rounded">
       <h6 class="collapse-header">Section : </h6>
-      <a class="collapse-item <?= ($activePage == '1') ? 'active':''; ?>" href="<?php echo $base_url?>home/section-one/1.php">1</a>
-      <a class="collapse-item <?= ($activePage == '2') ? 'active':''; ?>" href="<?php echo $base_url?>home/section-two/2.php">2</a>
+      <a class="collapse-item <?= ($activePage == '1') ? 'active' : ''; ?>" href="<?php echo $base_url ?>home/section-one/1.php">1</a>
+      <a class="collapse-item <?= ($activePage == '2') ? 'active' : ''; ?>" href="<?php echo $base_url ?>home/section-two/2.php">2</a>
     </div>
   </div>
 </li>
-<li class="nav-item <?= ($activePage == 'about') ? 'active':''; ?>">
-  <a class="nav-link" href="<?php echo $base_url?>about/about.php">
+<li class="nav-item <?= ($activePage == 'about') ? 'active' : ''; ?>">
+  <a class="nav-link" href="<?php echo $base_url ?>about/about.php">
     <i class="fa-solid fa-info"></i>
     <span>About</span></a>
 </li>
-<li class="nav-item <?= ($activePage == 'bisnis') ? 'active':''; ?>">
-  <a class="nav-link" href="<?php echo $base_url?>bisnis/bisnis.php">
+<li class="nav-item <?= ($activePage == 'bisnis') ? 'active' : ''; ?>">
+  <a class="nav-link" href="<?php echo $base_url ?>bisnis/bisnis.php">
     <i class="fa-solid fa-money-check"></i>
     <span>Bisnis</span></a>
 </li>
-<li class="nav-item <?= ($activePage == 'newsroom') ? 'active':''; ?>">
-  <a class="nav-link" href="<?php echo $base_url?>newsroom/newsroom.php">
+<li class="nav-item <?= ($activePage == 'newsroom') ? 'active' : ''; ?>">
+  <a class="nav-link" href="<?php echo $base_url ?>newsroom/newsroom.php">
     <i class="fa-solid fa-newspaper"></i>
     <span>Newsroom</span></a>
 </li>
-<li class="nav-item <?= ($activePage == 'gabung') ? 'active':''; ?>">
-  <a class="nav-link" href="<?php echo $base_url?>gabung/gabung.php">
+<li class="nav-item <?= ($activePage == 'gabung') ? 'active' : ''; ?>">
+  <a class="nav-link" href="<?php echo $base_url ?>gabung/gabung.php">
     <i class="fa-solid fa-users"></i>
     <span>Gabung</span></a>
 </li>
+<li class="nav-item <?= ($activePage == 'kontak') ? 'active' : ''; ?>">
+  <a class="nav-link" href="<?php echo $base_url ?>kontak/kontak.php">
+    <i class="fa-solid fa-address-card"></i>
+    <span>Kontak</span></a>
+</li>
+<li class="nav-item <?= ($activePage == 'lokasi') ? 'active' : ''; ?>">
+  <a class="nav-link" href="<?php echo $base_url ?>lokasi/lokasi.php">
+    <i class="fa-solid fa-location-dot"></i>
+    <span>Lokasi Kantor</span></a>
+</li>
 <li class="nav-item">
-  <a class="nav-link" href="<?php echo $landing_url?>home.php" target="_blank">
+  <a class="nav-link" href="<?php echo $landing_url ?>home.php" target="_blank">
     <i class="fa-solid fa-globe"></i>
     <span>Go To Website</span></a>
 </li>
